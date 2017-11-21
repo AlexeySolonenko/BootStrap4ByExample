ce24f4a8d066dd2c8e036762e1957aa3496dbb48
Alexey Solonenko <ot2@bk.ru>
2017-11-21 21:21:50 +0100

    Final result

---
+++ reflections.js
@@ -0,0 +1,10 @@
+
+
+20171120
+Sass an important pre-processors. Vars may be set with default values and thus a project be compiled.
+
+
+20171119
+Started the course. Like the teacher very much. 
+The first time installing bootstrap via npm.
+
